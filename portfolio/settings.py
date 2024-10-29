23dc2b5c4826a2c5467f233941dbd822db1d924f
prakash-cse <dvkprakash22122003@gmail.com>
2024-10-29 14:06:17 +0530

    added changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,20 +79,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'portfolio',
-        'USER':'postgres',
-        'PASSWORD':'1234',
-        'HOST':'localhost'
-    }
-}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfolio',
+        'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'PASSWORD': (os.environ['DB_PASSWORD_YO']),
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '23278',
     }
 }
 
