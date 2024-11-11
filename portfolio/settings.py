2c8309cc483893f852595dc35176dfd44a4372bb
prakash-cse <dvkprakash22122003@gmail.com>
2024-11-11 17:20:44 +0530

    added static file and changed to sqllite

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,15 +78,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': (os.environ['DB_PASSWORD_YO']),
-        'HOST': 'viaduct.proxy.rlwy.net',
-        'PORT': '8000',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
